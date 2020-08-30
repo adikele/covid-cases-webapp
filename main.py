84815bdb5d1e810efe4273c8b21de6666dcd5824
adikele <bajya@bajya-Air.(none)>
2020-08-30 18:55:28 +0300

    Adding linegraphs

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# This module has functions for creating bargraphs and linegraphs
+# The functions for bargraphs are written first, followed by those for linegraphs
+# Bargraphs show countries and cases for 1 user-entered country + 4 random countries
+# Linegraphs plot number of case (yaxis) versus time (xaxis) for 3 user-entered countries
+# .csv data file obtained from: https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data
+# Program created by Aditya Kelekar (adityakelekar@yahoo.com)
 import io
 import random
-
 import pandas as pd
-import random
 from flask import Flask, request, render_template, Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -11,6 +15,7 @@ DATA_FILE = "EUOpenData_24_8_2020"
 DATA_DATE = "24.8.2020"
 NEXT_UPDATE_DATE = "31.8.2020"
 global x, y
+
 app = Flask(__name__)
 
 
@@ -33,10 +38,10 @@ def input_validataion(name_country, dict_all_countries):
 
 
 def fetch_all_countries(df):
-    """ 
+    """
     Function finds the list of countries from all the continents
     It takes a dataframe of the input data file
-    Returns a dict with keys --> continents  
+    Returns a dict with keys --> continents
                       values --> list of countries in the respective continents  
     """
     continents = ("Africa", "Europe", "America", "Oceania", "Asia")
@@ -118,8 +123,74 @@ def create_figure(countries, cases):
     axis = fig.add_subplot(1, 1, 1)
     axis.bar(countries, cases, color="blue")
     axis.set_ylabel("Persons infected in last 14 days")
-    axis.set_title(f"Cumulative number (14 days) of COVID-19 cases per 100000 persons \n Data updated on: {DATA_DATE} Next update: {NEXT_UPDATE_DATE}")
+    axis.set_title(
+        f"Cumulative number (14 days) of COVID-19 cases per 100000 persons \n Data updated on: {DATA_DATE} Next update: {NEXT_UPDATE_DATE}"
+    )
     return fig
+
+
+# NOTE: processing functions for linegraphs start from here..
+def creating_date_list(df, n):
+    df1 = df[df["countriesAndTerritories"] == "Afghanistan"]
+    dict1_one_country = df1.to_dict()
+    date_list = list(dict1_one_country["dateRep"].values())
+    newlistdate_list = date_list[:n]  # take the last "n" days
+    newlistdate_list.reverse()  # reverse the list, now list ends with latest date
+    return newlistdate_list
+
+
+def fetch_all_continent_data(df):
+    """
+    Function finds the countries and cases for all continents
+    It takes a dataframe of the input data file 
+    Returns two things: (i) a list of countries 
+    (ii) a dict with keys --> countries 
+                   values --> list of cases for each of the days recorded since Dec 2019
+    """
+    # extracting the countries from all continents:
+    df3 = df[(df["dateRep"] == "28/07/2020")]
+    dict3_countries_of_all_continent = df3.to_dict()
+    countries_list = list(
+        dict3_countries_of_all_continent["countriesAndTerritories"].values()
+    )
+    z = len(countries_list)  # getting the number of countries
+
+    dict_countries_cases = dict()
+    for i in range(z):
+        df_current = df[df["countriesAndTerritories"] == countries_list[i]]
+        dict_current = df_current.to_dict()
+        dict_countries_cases[countries_list[i]] = list(
+            dict_current[
+                "Cumulative_number_for_14_days_of_COVID-19_cases_per_100000"
+            ].values()
+        )
+
+    return countries_list, dict_countries_cases
+
+
+def fetch_three_countries_data(
+    dict_countries_cases, country_sel1, country_sel2, country_sel3, n
+):
+    """
+    Function creates a dict of countries and cases for 3 countries
+    It takes (i) a dict of countries and cases (ii) 3 strings, country_sel
+    (iii) a list of 4 random countries
+    Returns a dict of countries and cases for 3 countries
+    """
+    # creating a dict of countries and cases for the 3 countries
+    dict_threecountries = dict()
+    dict_threecountries[country_sel1] = dict_countries_cases[country_sel1]
+    dict_threecountries[country_sel2] = dict_countries_cases[country_sel2]
+    dict_threecountries[country_sel3] = dict_countries_cases[country_sel3]
+
+    # within the dict, "process" the list of case numbers
+    for i in dict_threecountries.keys():
+        abc_real_list = dict_threecountries[i]
+        newabc_real_list = abc_real_list[:n]  # take the last "n" days
+        newabc_real_list.reverse()  # reverse the list, now list ends with latest cases
+        dict_threecountries[i] = newabc_real_list
+
+    return dict_threecountries
 
 
 @app.route("/")
@@ -172,6 +243,63 @@ def plot():
     global x
     global y
     fig = create_figure(x, y)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype="image/png")
+
+
+# NOTE: template and graph functions for linegraphs start from here..
+# plotting countries and cases over time:
+def create_figure_linegraphs(newlistdate_list, dict_three_countries):
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    for i in dict_three_countries.keys():
+        axis.plot(newlistdate_list, dict_three_countries[i], label=i)
+    axis.set_xticks(
+        ["15/04/2020", "15/05/2020", "15/06/2020", "15/07/2020", "15/08/2020"]
+    )
+    z = ["mid-April", "mid-May", "mid-June", "mid-July", "mid-August"]
+    axis.set_xticklabels(z)
+    axis.set_ylabel("Cumulative number of new virus infections per 100000 inhabitants")
+    axis.set_xlabel("Year 2020")  # doesn't work
+    axis.set_title("Covid-19 infections - Country Graphs")
+    axis.legend(loc="best")
+    return fig
+
+
+@app.route("/linegraphs_form")
+def linegraphs_form():
+    df = pd.read_csv(DATA_FILE)
+    dict_all_countries = fetch_all_countries(df)
+    list_all_countries = []
+    for i in dict_all_countries.values():
+        for j in i:
+            list_all_countries.append(j)
+    list_all_countries_sorted = sorted(list_all_countries)
+    return render_template("linegraphs_form.jinja2", options=list_all_countries_sorted)
+
+
+@app.route("/plot_linegraphs.png", methods=["POST"])
+def plot_linegraphs():
+    global x
+    global y
+    n = 140
+
+    country_sel1 = request.form["country_name1"]
+    country_sel2 = request.form["country_name2"]
+    country_sel3 = request.form["country_name3"]
+
+    df = pd.read_csv(DATA_FILE)
+
+    list_countries, dict_countries_cases = fetch_all_continent_data(df)
+
+    dict_three_countries = fetch_three_countries_data(
+        dict_countries_cases, country_sel1, country_sel2, country_sel3, n
+    )
+
+    newlistdate_list = creating_date_list(df, n)
+
+    fig = create_figure_linegraphs(newlistdate_list, dict_three_countries)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype="image/png")
